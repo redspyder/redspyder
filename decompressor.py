71a080a0a8fc2d5f5911c9bab1c106af80465566
Lara <martilar@onid.orst.edu>
2013-05-08 11:36:28 -0700

    decompressor.py now running

--- decompressor.py
+++ decompressor.py
@@ -3,17 +3,11 @@
 import sys
 import os
 import tarfile
-import bz2
 
 def decompress(path):
 	output = open("output.txt", "w")
 	sys.stdout = output
 	sys.stderr = output
-	
-	dir_list = []
-	dir_path = ""
-	file_path = ""
-	file_name = ""
 	
 	if os.path.isdir(path):
 		dir_list = os.listdir(path)
@@ -22,14 +16,9 @@ def decompress(path):
 			decompress(dir_path)
 	
 	elif os.path.isfile(path):
-		file_name = os.path.basename(path)
 		dir_path = os.path.dirname(path)
-		
-		if (path.endswith('.tar.bz2')):
-			data = bz2.BZ2File(file_name)
-			bz2.decompress(data)
 			
-		if (tarfile.is_tarfile(file_name)):
-			tarfile.open(path).extractall(dir_path)
+		if (tarfile.is_tarfile(path)):
+			tarfile.open(path, 'r:bz2').extractall(dir_path)
 
 	output.close()
