0fb7e5786f2c20346b52f887559be1d9582b5338
Lara <martilar@onid.orst.edu>
2013-05-08 12:57:44 -0700

    Fixed typos

--- autograder.py
+++ autograder.py
@@ -10,6 +10,10 @@ args = argumentParser.parseArgs(sys.argv)
 
 print args
 
+## if no argument for stage provided, assume running all
+if args.stage == []:
+	args.stage = [1, 2, 3, 4]
+	
 ## all student folders in homework folder
 students = os.listdir(args.hw_folder)
 
@@ -22,7 +26,7 @@ if 1 in args.stage:
 ## Stage 2 = compilation
 if 2 in args.stage:
 	for each s in students:
-		agCompile(students)
+		agCompile(args.hw_folder, s)
 	## if error that prevents continuation occurs, exit program
 
 ## Stage 3 = testing
--- testCompiler.py
+++ testCompiler.py
@@ -1,13 +1,15 @@
-import agCompile.py
+import agCompile
 import unittest
+import os
 
+CURDIR = os.getcwd()
 
 class PositiveTestCase(unittest.TestCase):
 	def runTest(self):
-	
+		agcompile(CURDIR, "fakestring")	
 class NegativeTestCase(unittest.TestCase):
 	def runTest(self):
-		
+		agcompile(CURDIR, "fakestring")
 		
 if __name__ == '__main__':
 	unittest.main()
