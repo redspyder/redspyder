87ae02ad4115e523da200c1af33ddf17c74bc284
Jeff Toy <jtoy@yahoo.com>
2013-05-14 11:25:05 -0700

    Generic Class for getting a list of subfolders

---
+++ agGetSubFolders.py
@@ -0,0 +1,39 @@
+import os
+
+##################################################################################################################################
+# Class agGetSubFolders
+# Author: Jeff Toy
+# Description:
+#   Class for creating a list of subfolders in a folder.
+# Input:
+#   Top level folder
+# Output:
+#   List of subfolders in top level folder.  Folders are absolute paths.
+# Usage:
+#   See sample usage at bottom of file.  Uncomment and run this file to demonstrate.
+##################################################################################################################################
+
+class agGetSubFolders:
+
+    def __init__(self,folder):
+        assert(folder != "")
+        self.topFolder = folder
+        currentFolder = os.getcwd()
+        if(not os.path.isdir(self.topFolder)):
+            print("Invalid input folder.")
+            return
+        self.folderList = []
+        os.chdir(self.topFolder)
+        folderContents = os.listdir("./")
+        for f in folderContents:
+            if(os.path.isdir(f)):
+                self.folderList.append(os.getcwd()+"/"+f)
+        os.chdir(currentFolder)
+
+    def getFolders(self):
+        return(sorted(self.folderList))
+        
+## example usage:
+#r = agGetSubFolders("HW3")
+#folders = r.getFolders()
+#print(folders)        
