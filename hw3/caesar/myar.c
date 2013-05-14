3bb9501d69464d4f867a3b5ac41067bdc6068b7e
xeodot <redspyder>
2013-05-14 08:24:44 -0400

    renamed students shorter

--- hw3/caesar/myar.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("This is Caesar's test program.");
-
-    return 0;
-}
--- hw3/houdini/myar.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("This is Houdini's test program.");
-
-    return 0;
-}
