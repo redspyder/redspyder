04edaac5afdd1c5aa16499f0641788adfbf6a3ef
Lara <martilar@onid.orst.edu>
2013-05-15 14:28:12 -0700

    argumentParser instantiated as a class, autograder updated to reflect changes to argumentParser

--- argumentParser.py
+++ argumentParser.py
@@ -1,21 +1,35 @@
 import argparse
-import sys
+import os
 
-def parseArgs(args):
-    """Parses the command line arguments
-    Keyword arguments:
-    args -- array of arguments
-	"""
-    parser = argparse.ArgumentParser(description="Autograde assignments", prog="autograder")
-    parser.add_argument('hw_folder', metavar='hw_folder', help='folder containing homework assignments')
-    parser.add_argument("--stage", help="which stage of autograder to run", type=int, action="append")
-    parser.add_argument("--timeout", help="timeout for autograder (in minutes)", type=int)
-    parser.add_argument("--test", help="test file to run on assignment", action="append", type=argparse.FileType('r'))
-    parser.add_argument("--template", help="template file submitted by students", action="append", type=argparse.FileType('r'))
-    args = parser.parse_args()
-    
-    return args
+class ParseArgs:
+	def __init__(self):
+		parser = argparse.ArgumentParser(description="Autograde assignments", prog="autograder")
+		parser.add_argument('hw_folder', metavar='hw_folder', help='folder containing homework assignments')
+		parser.add_argument('--stage', help='which stage of autograder to run', type=int, action='append')
+		parser.add_argument('--timeout', help='timeout for autograder (in minutes)', type=int)
+		parser.add_argument('--test', help='test file to run on assignment', action='append', type=argparse.FileType('r'))
+		parser.add_argument('--template', help='template file submitted by students', action='append', type=argparse.FileType('r'))
+		args = parser.parse_args()
+		if (args.stage):
+			self.stage = args.stage
+		else:
+			self.stage = [1, 2, 3, 4]
+		self.hw_folder = args.hw_folder
+		self.timeout = args.timeout
+		self.test = args.test
+		self.template = args.template
+		
+	def getFolder(self):
+		return self.hw_folder
 	
-if __name__ == '__main__':
-    args = parseArgs(sys.argv)
-    print args
+	def getStage(self):
+		return self.stage
+	
+	def getTimeout(self):
+		return self.timeout
+		
+	def getTest(self):
+		return self.test
+	
+	def getTemplate(self):
+		return self.template
--- autograder.py
+++ autograder.py
@@ -1,33 +1,43 @@
 import argumentParser
 import sys
-import stage1
-import stage2
-import stage3
-import stage4
+##import stage1
+##import stage2
+##import stage3
+##import stage4
 
 
-args = argumentParser.parseArgs(sys.argv)
+args = argumentParser.ParseArgs()
 
 #print args
 
 ## if no argument for stage provided, assume running all
-if args.stage == []:
+if args.getStage() == []:
 	args.stage = [1, 2, 3, 4]
 
 ## Stage 1 = decompression
-if 1 in args.stage:
-    stage1.run(args.hw_folder)
-## if error that prevents continuation occurs, exit program
+if 1 in args.getStage():
+    print "---stage1---\n"
+    print args.getFolder()
+    ##stage1.run(args.getFolder())
+	## if error that prevents continuation occurs, exit program
 	
 ## Stage 2 = compilation
-if 2 in args.stage:
-	stage2.run(args.hw_folder)
+if 2 in args.getStage():
+	print "---stage2---\n"
+	print args.getFolder()
+	##stage2.run(args.getFolder())
 	## if error that prevents continuation occurs, exit program
 
 ## Stage 3 = testing
-if 3 in args.stage:
-    stage3.run(args.hw_folder, args.timeout, args.test)
+if 3 in args.getStage():
+	print "---stage2---\n"
+	print args.getFolder()
+	print args.getTimeout()
+	print args.getTest()
+    ##stage3.run(args.getFolder(), args.getTimeout(), args.getTest())
 
 ## Stage 4 = reporting
-if 4 in args.stage:
-	stage4.run(args.hw_folder)
+if 4 in args.getStage():
+	print "---stage4---\n"
+	print args.getFolder()
+	##stage4.run(args.getFolder())
