b3850fce9b6daffcca516c749b85577c06d73aaa
Jeff Toy <jtoy@yahoo.com>
2013-05-20 21:29:39 -0700

    Separated uploader from reporter.
    Modified uploader and reporter to work.

---
+++ reporter.py
@@ -0,0 +1,33 @@
+#! /user/bin/python
+
+import MySQLdb as mdb
+import sys
+import subprocess
+
+def reporter(student, assn):
+	try:
+		con = mdb.connect(host='mysql.cs.orst.edu', user='cs419_haroldl', passwd='8091', db='cs419_haroldl')
+		cursor = con.cursor()
+		cursor.execute("SELECT stage1, stage2, stage3 FROM Report WHERE sid='%s' AND aid='%s'" % (student, assn))
+		
+		rows = cursor.fetchall()
+		fd = open('report.txt','a')
+		for itm in rows:
+		    fd.write('---------------------------------------------------------------------------------------\n')
+		    fd.write("Student: %s:\n" % student)
+		    fd.write("Stage 1:\n")
+		    fd.write("%s\n" % itm[0])
+		    fd.write("Stage 2:\n")
+		    fd.write("%s\n" % itm[1])
+		    fd.write("Stage 3:\n")
+		    fd.write("%s\n" % itm[2])
+		    fd.write('---------------------------------------------------------------------------------------\n')
+
+		fd.close()
+		
+	except (mdb.Error, IOError) as e:
+		print "Error %d: %s" % (e.args[0], e.args[1])
+
+
+reporter("jack", "HW2")
+		
--- uploader.py
+++ uploader.py
@@ -16,29 +16,25 @@ def uploader(student, assn, stage, upload_file):
 
 	try:
 		con = mdb.connect('mysql.cs.orst.edu', 'cs419_haroldl',
-		'8091', 'group6_db')
+		'8091', 'cs419_haroldl')
 	
 		cursor = con.cursor()
-		cursor.execute("SELECT * FROM Reports WHERE sid='student' AND aid='assn'")
-		numrows = cursor.rowcount
-		if rowcount < 1:
-			cursor.execute("INSERT INTO Reports ('%s') VALUES('%s')\
-                        WHERE sid = '%s' AND aid = '%s'" %
-                        mdb.escape_string(stage), mdb.escape_string(upload),
-                        student, assn)
+		cursor.execute("SELECT * FROM Report WHERE sid='%s' AND aid='%s'" % (student, assn))
+		rows = cursor.fetchall()
+	
+		if len(rows) < 1:
+			cursor.execute("INSERT INTO Report(sid, aid, %s) VALUES('%s','%s','%s')" % (stage,student, assn, mdb.escape_string(upload)))
 		else:
-			cursor.execute("UPDATE Reports SET '%s' = '%s'\
-                        WHERE sid = '%s' AND aid = '%s'" %
-                        mdb.escape_string(stage), mdb.escape_string(upload),
-                        student, assn)
+			cursor.execute(("UPDATE Report SET %s = '%s' WHERE sid = '%s' AND aid = '%s'" % (stage, mdb.escape_string(upload), student, assn)))
 		con.commit()
 		
 		cursor.close()
 		
 	except mdb.Error as e:
 		print "Error %d: %s" % (e.args[0], e.args[1])
-		
 
+uploader("jack","HW2", "stage1", "stage1.out")		
+"""
 def reporter(student, assn):
 	try:
 		con = mdb.connect(host='mysql.cs.orst.edu', user='cs419_haroldl',
@@ -61,3 +57,6 @@ def reporter(student, assn):
 		
 	except (mdb.Error, IOError) as e:
 		print "Error %d: %s" % (e.args[0], e.args[1])
+
+"""		
+		
